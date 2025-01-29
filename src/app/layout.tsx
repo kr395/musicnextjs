304a0d4092374248e1502473237c8b7487e8d5d6
Gopal Kumar Das <krgopalkumar706@gmail.com>
2025-01-29 11:00:00 +0530

    Herro Section Completed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "../components/Navbar";
+import HeroSection from "@/components/HeroSection";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,7 @@ export default function RootLayout({
       >
         <div className="relative w-full flex item-center justify-center ">
            <Navbar/>
+           <HeroSection/>
           </div> 
         {children}
       </body>
--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,20 +1,30 @@
 import Link from "next/link";
+import { Spotlight } from "../components/ui/Spotlight";
+import { Button } from "./ui/moving-border";
 
-export default function HeroSection(){
-    return(
-        <div
-         className="h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0"
-        >
-            <div className="p-4 relative z-10 w-full text-center">
-            <h1>
-                Master the art of Music
-                </h1>
-               <p> Dive into our comprehensive music courses And transform your musical journey today. Whether you are a beginner Or looking to refine your skills, Join us to unlock your true potential </p>
-               <div className="mt-4">
-                <Link href={"/courses"}>Explore Courses</Link>
-               </div>
-            </div>
-            
+export default function HeroSection() {
+  return (
+    <div className="h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0">
+      <Spotlight
+        className="-top-40 left-0 md:left-60 md:-top-20"
+        fill="#34ebb1"
+      />
+      <div className="p-4 relative z-10 w-full text-center">
+        <h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">
+          Master the art of Music
+        </h1>
+        <p className="mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto">
+          {" "}
+          Dive into our comprehensive music courses And transform your musical
+          journey today. Whether you are a beginner Or looking to refine your
+          skills, Join us to unlock your true potential{" "}
+        </p>
+        <div className="mt-4">
+          <Link href={"/courses"}>
+            <Button borderRadius="1.75rem" className="">Explore Course </Button>
+          </Link>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
