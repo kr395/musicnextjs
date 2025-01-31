88355f99992c2a4068b2b489738aa95d25c79075
Gopal Kumar Das <krgopalkumar706@gmail.com>
2025-01-31 11:00:00 +0530

    WhyUs Section with sticky scrool is completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import FeaturedSection from "@/components/FeaturedSection";
 import HeroSection from "@/components/HeroSection";
+import WhyUS from "@/components/WhyUS";
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <HeroSection/>
       <FeaturedSection/>
+      <WhyUS/>
     </main>
   );
 }
