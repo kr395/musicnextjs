40148a46b9d2b128650bc4e32e4fba0b38dc081d
Gopal Kumar Das <krgopalkumar706@gmail.com>
2025-02-01 11:00:00 +0530

    Webinars Section with hover effect is completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import FeaturedSection from "@/components/FeaturedSection";
 import HeroSection from "@/components/HeroSection";
 import Testimonials from "@/components/Testimonials";
+import UpcomingWebinars from "@/components/UpcomingWebiners";
 import WhyUS from "@/components/WhyUS";
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <FeaturedSection/>
       <WhyUS/>
       <Testimonials/>
+      <UpcomingWebinars/>
     </main>
   );
 }
